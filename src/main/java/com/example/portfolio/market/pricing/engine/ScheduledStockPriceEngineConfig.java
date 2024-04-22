60dc442880ca8bb7b186eed67b407cb55b1f9b88
Eli <elicodesforfun@gmail.com>
2024-04-22 16:02:36 +0800

    inline class

--- src/main/java/com/example/portfolio/market/pricing/engine/ScheduledStockPriceEngineConfig.java
+++ src/main/java/com/example/portfolio/market/pricing/engine/ScheduledStockPriceEngineConfig.java
@@ -1,15 +1,21 @@
 package com.example.portfolio.market.pricing.engine;
 
 import com.example.portfolio.market.pricing.store.PriceStore;
+import com.example.portfolio.security.Stock;
 import com.example.portfolio.security.StockRepository;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.Trigger;
+import org.springframework.scheduling.TriggerContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
+import java.time.Instant;
+import java.util.Date;
+import java.util.Optional;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
@@ -25,22 +31,13 @@ import java.util.concurrent.Executors;
 @EnableScheduling
 class ScheduledStockPriceEngineConfig implements SchedulingConfigurer {
 
-    private static final int MIN_INTERVAL_MS = 500;
-    private static final int MAX_INTERVAL_MS = 2000;
-
-    private final ApplicationEventPublisher applicationEventPublisher;
-    private final PriceStore priceStore;
     private final StockRepository stockRepository;
     private final ScheduledStockPriceEngine engine;
 
     ScheduledStockPriceEngineConfig(
-        final ApplicationEventPublisher applicationEventPublisher,
-        final PriceStore priceStore,
         final StockRepository stockRepository,
         final ScheduledStockPriceEngine engine
     ) {
-        this.applicationEventPublisher = applicationEventPublisher;
-        this.priceStore = priceStore;
         this.stockRepository = stockRepository;
         this.engine = engine;
     }
@@ -59,5 +56,48 @@ class ScheduledStockPriceEngineConfig implements SchedulingConfigurer {
             Tick tick = new Tick(engine, stock);
             taskRegistrar.addTriggerTask(tick, tick);
         });
+    }
+
+    /**
+     * A scheduled task that updates the price of a stock at random intervals.
+     */
+    private static class Tick implements Runnable, Trigger {
+
+        private final PriceEngine engine;
+
+        private final Stock stock;
+
+        private TimeStep timeStep;
+
+        public Tick(PriceEngine engine, Stock stock) {
+            this.engine = engine;
+            this.stock = stock;
+            this.timeStep = TimeStep.fromNow();
+        }
+
+        @Override
+        public void run() {
+            engine.onTick(stock, timeStep);
+        }
+
+        @Override
+        public Date nextExecutionTime(TriggerContext context) {
+            Optional<Date> lastCompletionTime = Optional.ofNullable(context.lastCompletionTime());
+
+            // Advance the time step by a random interval between 0.5s - 2s
+            int deltaT = randBetween(500, 2000);
+            timeStep = timeStep.advance(deltaT);
+
+            Instant nextExecutionTime =
+                    lastCompletionTime.orElseGet(Date::new)
+                            .toInstant()
+                            .plusMillis(deltaT);
+
+            return Date.from(nextExecutionTime);
+        }
+
+        private int randBetween(int start, int end) {
+            return start + (int) Math.round(Math.random() * (end - start));
+        }
     }
 }
